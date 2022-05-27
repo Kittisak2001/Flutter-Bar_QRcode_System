1801c865feb86d04d30a4fcc1216f18a5042e77f
Kittisak Phatchaiphongsa <kittisak.ph2001@gmail.com>
2022-05-27 16:45:43 +0700

    Pixel4 No Phone real

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:qrscan/qrscan.dart' as scanner;
+import 'package:url_launcher/url_launcher.dart';
 
 void main() {
   runApp(const MyApp());
@@ -6,41 +8,20 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.brown,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Scan Bar_QR Code'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -48,68 +29,119 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  String? scanresult;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+  bool checkLineURL = false, checkFacebookURL = false, checkYoutubeURL = false;
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+    checkLineURL = true; checkFacebookURL = true; checkYoutubeURL = true;
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Container(
+          child: SizedBox(
+              height: 300,
+              width: double.infinity,
+              child: Card(
+                child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        "ผลการสแกน",
+                        style: TextStyle(fontSize: 25),
+                      ),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      Text(
+                        scanresult ??= "ยังไม่มีข้อมูล",
+                        style: TextStyle(fontSize: 25),
+                      ),
+                      Spacer(),
+                      checkLineURL
+                          ? SizedBox(
+                              width: double.infinity,
+                              child: RaisedButton(
+                                onPressed: () async{
+                                  scanresult = "https://line.me/ti/p/wsRCNoGovZ";
+                                  launch(scanresult!);
+                                },
+                                color: Colors.green[900],
+                                child: Text(
+                                  "Open Line",
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                  ),
+                                ),
+                              ),
+                            )
+                          : Container(),
+                      Spacer(),
+                      checkFacebookURL
+                          ? SizedBox(
+                              width: double.infinity,
+                              child: RaisedButton(
+                                onPressed: () async{
+                                  scanresult = "https://facebook.com/qr?id=100009272447378";
+                                  launch(scanresult!);
+                                  
+                                },
+                                color: Colors.blue[900],
+                                child: Text(
+                                  "Open Facebook",
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                  ),
+                                ),
+                              ),
+                            )
+                          : Container(),
+                      Spacer(),
+                      checkYoutubeURL
+                          ? SizedBox(
+                              width: double.infinity,
+                              child: RaisedButton(
+                                onPressed: () async{
+                                  scanresult = "https://www.youtube.com/channel/UCKWp4QbICmcrLN5vNphcCWQ";
+                                  launch(scanresult!);
+                                },
+                                color: Colors.red[900],
+                                child: Text(
+                                  "Open Youtube",
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                  ),
+                                ),
+                              ),
+                            )
+                          : Container(),
+                    ]),
+              )),
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+        onPressed: startScan,
+        child: Icon(Icons.qr_code_scanner_sharp),
+      ),
     );
+  }
+
+  startScan() async {
+    String? cameraScanResult = await scanner.scan();
+    setState(() {
+      scanresult = cameraScanResult;
+    });
+
+    if (scanresult!.contains("line.me")) {
+      checkLineURL = true;
+    }else if(scanresult!.contains("facebook.com")){
+      checkFacebookURL = true;
+    }else if(scanresult!.contains("youtube.com")){
+      checkYoutubeURL = true;
+    }
   }
 }
